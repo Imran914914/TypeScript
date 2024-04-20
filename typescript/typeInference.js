5071a64da6c9e49417e2f377b1d5eb63c2779bae
Imran Ali <167523906+Imran914914@users.noreply.github.com>
2024-04-20 10:47:42 +0000

    Done

---
+++ typescript/typeInference.js
@@ -0,0 +1,16 @@
+"use strict";
+// number
+Object.defineProperty(exports, "__esModule", { value: true });
+var userId = 112233;
+userId.toFixed();
+//instead
+// let userId= 112233;
+// userId.toFixed();
+// boolean 
+var isLoggedIn = false;
+isLoggedIn.valueOf();
+//instead
+// let isLoggedIn= false;
+// isLoggedIn.valueOf();
+var userName = "imran";
+userName.length;
---
+++ typescript/typeInference.ts
@@ -0,0 +1,29 @@
+// number
+
+let userId: number = 112233;
+userId.toFixed();
+
+//instead
+// let userId= 112233;
+// userId.toFixed();
+
+// boolean 
+
+let isLoggedIn: boolean = false;
+isLoggedIn.valueOf();
+
+//instead
+// let isLoggedIn= false;
+// isLoggedIn.valueOf();
+
+let userName: string = "imran";
+userName.length;
+
+//instead
+// let userName= "imran";
+// userName.length;
+
+
+//typescript is smart enough to inference or recognise the types of variables when value assigned to them immediately 
+
+export{}
