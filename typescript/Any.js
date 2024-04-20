d35e472ed8ee2961444b2b959c2a18039e57f9d6
Imran Ali <167523906+Imran914914@users.noreply.github.com>
2024-04-20 13:08:53 +0000

    Done

---
+++ typescript/Any.js
@@ -0,0 +1,10 @@
+// let hero;
+// function getHero(){
+//     return "thor";
+// }
+// hero = getHero();
+var hero;
+function getHero() {
+    return 454;
+}
+hero = getHero();
---
+++ typescript/Any.ts
@@ -0,0 +1,47 @@
+// Don't use 'any' type because we will not face an error while returning value thats actually an error
+// and there will be chance to unable to find error. So avoid using 'any'.........
+
+
+
+
+
+
+
+// let hero;
+
+// function getHero(){
+//     return "thor";
+// }
+
+// hero = getHero();
+
+
+
+
+// let hero: string;
+
+// function getHero(){
+//     return 454;
+// }
+
+// hero = getHero();
+
+
+// let hero: string;
+
+// function getHero(){
+//     return null;
+// }
+
+// hero = getHero();
+
+
+// let hero: string;
+
+// function getHero(){
+//     return true;
+// }
+
+// hero = getHero();
+
+export{};
