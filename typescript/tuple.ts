a4c04014ee33ba8526232ec734ffcab5664ba089
Imran Ali <167523906+Imran914914@users.noreply.github.com>
2024-04-23 04:43:18 +0000

    Done

---
+++ typescript/tuple.ts
@@ -0,0 +1,18 @@
+// // const user: (string|number)[]=["hello", 1,2]
+// let user: [string, number, boolean];
+// user=["hello", 1,true]
+// user=["hi",2,false]
+
+// let rgb:[number, number, number] = [234,123,214];
+// type RGB=[number, number, number];
+
+// let newrgb:RGB;
+// newrgb=[123,123,123]
+
+// type User = [number, string];
+
+// const newUser: User = [123,"userName"];
+// newUser[1] = "abc@test.com";
+// newUser.push(true);
+
+// export {}
