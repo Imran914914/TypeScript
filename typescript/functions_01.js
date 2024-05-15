3ffa1e018f3dff7797f97dfcdc5a0c7fab169dce
Imran Ali <167523906+Imran914914@users.noreply.github.com>
2024-05-15 08:42:55 +0000

    Done

--- typescript/functions_01.js
+++ typescript/functions_01.js
@@ -1,6 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
+    // num.toUpperCase()
     return num + 2;
 }
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, password) {
+    console.log("name is : ".concat(name, " \nemail is : ").concat(email, " \npassword is : ").concat(password));
+}
+signUpUser("imran", "imran@gmail.com", 11223344);
+console.log(getUpper("imran"));
 console.log(addTwo(5));
--- typescript/functions_01.ts
+++ typescript/functions_01.ts
@@ -1,6 +1,19 @@
-function addTwo(num) {
+function addTwo(num:number) {
+    // num.toUpperCase()
     return num+2;
 }
+
+function getUpper(val:string){
+    return val.toUpperCase()
+}
+
+function signUpUser(name:string, email:string, password:string|number){
+    console.log(`name is : ${name} \nemail is : ${email} \npassword is : ${password}`)
+}
+
+signUpUser("imran","imran@gmail.com",11223344)
+
+console.log(getUpper("imran"))
 
 console.log(addTwo(5));
 
